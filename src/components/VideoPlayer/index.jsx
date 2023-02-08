7f722783946f3100c958edac60042d404a2cb116
Alexis Toribio <mtoribio@sanms.com.pe>
2023-02-08 12:13:41 -0500

    chore(Root): Modify button and SRC of VideoPlayer

--- src/components/VideoPlayer/index.jsx
+++ src/components/VideoPlayer/index.jsx
@@ -1,25 +1,23 @@
 import { useState, useRef } from "react";
 import styles from "./styles.module.css";
-import SRC from "../../../assets/image.jpg";
+import SRC from "../../../assets/pexels-cottonbro-5473800.mp4";
 
 export default function VideoPlayer() {
   const [playing, setPlaying] = useState(false);
   const video = useRef();
-  const handlePlay = () => {
+  const handlePlay = async () => {
     if (playing) {
       video.current.pause();
     } else {
       video.current.play();
     }
-
     setPlaying(!playing);
   };
 
   return (
-    //<video className={styles.video} src={SRC} controls={false} />
     <div>
-      <img className={styles.video} src={SRC} alt="image" ref={video} />
-      <button className={styles.player} onClick={handlePlay}></button>
+      <video className={styles.video} src={SRC} controls={false} ref={video} />
+      <button className={styles.player} onClick={handlePlay} />
     </div>
   );
 }
